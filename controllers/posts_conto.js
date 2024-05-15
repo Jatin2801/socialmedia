6bbb52e3cba635ae2bd4b8f641f7a2fd4e440f72
Jatin Verma <jatin28jat@gmail.com>
2024-05-15 12:44:58 +0530

    Post Deleting Added

--- controllers/posts_conto.js
+++ controllers/posts_conto.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post')
+const Comment = require('../models/comment')
 
 module.exports.create = function(req,res){
     Post.create({
@@ -15,3 +16,68 @@ module.exports.create = function(req,res){
     });
     
 }
+//Deleting Post 
+// module.exports.destroy = function(res,res){
+//     // First find if Post exists 
+//     Post.findById(req.params.id , function(err,post){
+//         //.id means conver. the object id into string
+//         if(post.user == req.user.id){
+//          post.remove()   
+//          Comment.deleteMany({post : req.params.id}
+//             .catch((err) => {
+//                 return res.redirect('back')
+//             })
+//             .then(()=>{
+//                 return res.redirect('back')
+//             })
+//          )
+//         }
+//     })
+// }
+
+// module.exports.destroy = function (req, res) {
+//     // First find if Post exists
+//     Post.findById(req.params.id)
+//         .then((post) => {
+//             //.id means converting the object id into a string
+//             if (post.user == req.user.id) {
+//                 return post.remove();
+//             } else {
+//                 throw new Error("Unauthorized");
+//             }
+//         })
+//         .then(() => {
+//             return Comment.deleteMany({ post: req.params.id });
+//         })
+//         .then(() => {
+//             return res.redirect('back');
+//         })
+//         .catch((err) => {
+//             console.error(err);
+//             return res.redirect('back');
+//         });
+// };
+
+module.exports.destroy = function (req, res) {
+    Post.findById(req.params.id)
+        .then((post) => {
+            if (!post) {
+                throw new Error('Post not found');
+            }
+            if (post.user.toString() !== req.user.id) {
+                throw new Error("Unauthorized access");
+            }
+            return Post.deleteOne({ _id: post._id });  // Using deleteOne instead of remove
+        })
+        .then(() => {
+            // Proceed to delete all comments associated with the post
+            return Comment.deleteMany({ post: req.params.id });
+        })
+        .then(() => {
+            res.redirect('back');
+        })
+        .catch((err) => {
+            console.error('Error:', err.message);
+            res.status(500).send(err.message);
+        });
+};
--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,8 @@ const postController = require('../controllers/posts_conto')
 
 router.post('/create' , passport.checkAuthentication  ,postController.create) // checkAuthentication is create in config pass-local-stra
 //checkAuthentication is middleware here 
+
+router.get('/destroy/:id',passport.checkAuthentication , postController.destroy) // :id is id of user here 
+
 module.exports = router; // this will be required in index file in routes folder
 //bcz of checkAuthentication anyone cannot edit html and create post via inspect
